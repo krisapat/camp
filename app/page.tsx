97dade1c092327744cc5dde2032ff148ea2db234
krisapat <pluemkrisapat@gmail.com>
2025-09-10 23:07:03 +0700

    อัปเดตโค้ดใหม่

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: "Camping",
   description: "Favorite your camping",
 };
-const Page = async ({ searchParams }:{ searchParams: { search?: string, category?: string } }) => {
+const Page = async ({ searchParams }:{ searchParams: Promise<{ search?: string, category?: string }> }) => {
   // Search
   console.log(searchParams)
   const { search, category } = await searchParams
--- components/Navbar/Navbar_Components/Search.tsx
+++ components/Navbar/Navbar_Components/Search.tsx
@@ -1,28 +1,29 @@
 'use client'
+
 import { Input } from "@/components/ui/input"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import { useDebouncedCallback } from 'use-debounce';
 
 const Search = () => {
-  const searchParams = useSearchParams()
-  const { replace } = useRouter()
-  const [search, setSearch] = useState(searchParams.get("search")?.toString() || "")
-  const handleSearch = useDebouncedCallback((value: string) => {
-    const params = new URLSearchParams(searchParams)
-    if (value) {
-      params.set("search", value)
-    } else {
-      params.delete("search")
-    }
-    replace(`?${params.toString()}`)
-  }, 500)
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [search, setSearch] = useState(searchParams.get("search") || "");
 
-  useEffect(() => {
-    if (!searchParams.get("search")) {
-      setSearch("")
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
     }
-  }, [searchParams.get("search")])
+    router.replace(`?${params.toString()}`);
+  }, 500);
+
+  const searchQuery = searchParams.get("search") || "";
+  useEffect(() => {
+    setSearch(searchQuery);
+  }, [searchQuery]);
 
   return (
     <Input
@@ -30,11 +31,12 @@ const Search = () => {
       placeholder="Search..."
       className="max-w-2xl"
       onChange={(e) => {
-        setSearch(e.target.value)
-        handleSearch(e.target.value)
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
       }}
       value={search}
     />
-  )
+  );
 }
-export default Search
+
+export default Search;
